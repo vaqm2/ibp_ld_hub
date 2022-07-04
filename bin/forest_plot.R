e2af059b55b6c32481064f8f49062c4a09cb4865
vaqm2 <appadurai.vivek@gmail.com>
2022-07-04 11:34:26 +0200

    plot code

--- bin/forest_plot.R
+++ bin/forest_plot.R
@@ -11,6 +11,9 @@ rG_table = rG_table %>%
     select(p1, p2, rg, rg_low, rg_high, p) %>% 
     mutate(p_fdr = p.adjust(p, method = c("fdr"), n = nrow(rG_table))) %>%
     mutate(Significant = ifelse(p_fdr < 0.05, "Yes", "No"))
+
+rG_table$p1 = gsub(".*$", "", rG_table$p1)
+rG_table$p2 = gsub(".*$", "", rG_table$p2)
     
 png(paste0(out_prefix, "_ldsc_rg.png"), 
     width = 10, 
