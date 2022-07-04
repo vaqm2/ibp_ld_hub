701cb36fa92b89c9ecb2f5db1de5dd1e48d6b3b5
vaqm2 <appadurai.vivek@gmail.com>
2022-07-04 13:05:50 +0200

    debug

--- bin/forest_plot.R
+++ bin/forest_plot.R
@@ -12,8 +12,8 @@ rG_table = rG_table %>%
     mutate(p_fdr = p.adjust(p, method = c("fdr"), n = nrow(rG_table))) %>%
     mutate(Significant = ifelse(p_fdr < 0.05, "Yes", "No"))
 
-rG_table$p1 = gsub(".*$", "", rG_table$p1)
-rG_table$p2 = gsub(".*$", "", rG_table$p2)
+rG_table$p1 = gsub("\..*$", "", rG_table$p1)
+rG_table$p2 = gsub("\..*$", "", rG_table$p2)
     
 png(paste0(out_prefix, "_ldsc_rg.png"), 
     width = 10, 
@@ -29,6 +29,6 @@ ggplot(rG_table, aes(x = rg, y = p2, color = Significant)) +
     geom_vline(xintercept = 0, lty = 2) +
     theme_bw() +
     scale_color_manual(values = c("red", "blue")) +
-    labs(caption = p1)
+    labs(caption = out_prefix)
 
 dev.off()
